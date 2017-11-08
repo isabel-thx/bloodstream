49df806fc5802b2d7b4d1a6edc5c4b31a901b4be
isabel-thx <isabelhx.teh@gmail.com>
2017-11-08 15:34:52 +0800

    user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,28 @@ class UsersController < ApplicationController
 	before_action :allowed?, only: [:verify]
 	before_action :set_user, only: [:show, :edit, :update, :upgrade, :downgrade]
 
+	def user_from_params
+  	first_name = user_params.delete(:first_name)
+  	last_name = user_params.delete(:last_name)
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+    blood_type = user_params.delete(:blood_type)
+    date_of_birth = user_params.delete(:date_of_birth)
+    phone_number = user_params.delete(:phone_number)
+    address = user_params.delete(:address)
+
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.first_name = first_name
+      user.last_name = last_name
+      user.email = email
+      user.password = password
+      user.blood_type = blood_type
+      user.date_of_birth = date_of_birth
+      user.phone_number = phone_number
+      user.address = address
+    end
+  end
+
 	def index
 		@users = User.all
 	end
@@ -59,6 +81,8 @@ class UsersController < ApplicationController
 
 	private
   def user_params
+    params[Clearance.configuration.user_parameter] || Hash.new
+
     params.require(:user).permit(:first_name, :last_name, :email, :blood_type, :date_of_birth, :email, :phone_number, :address, :verified, :points)
   end
 
